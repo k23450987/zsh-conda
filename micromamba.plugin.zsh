72b0a5c1e4d6e783af239aefb04ec5aa0f889aa9
Tom <xiaoti008@gmail.com>
2024-09-13 19:05:39 +0800

    Update micromamba.plugin.zsh

--- micromamba.plugin.zsh
+++ micromamba.plugin.zsh
@@ -1,7 +1,7 @@
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
 export MAMBA_EXE='/opt/homebrew/opt/micromamba/bin/micromamba';
-export MAMBA_ROOT_PREFIX='/Users/duket/micromamba';
+export MAMBA_ROOT_PREFIX='/Users/duket/.micromamba';
 __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
